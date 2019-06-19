861578c026964e636a6f866b9c6b7056325fa7bf
Helio <hbarroco@ciandt.com>
2019-06-19 16:52:46 -0300

    PoCs JWT + Cookies

---
+++ MvcCookieAuthentication/MvcCookieAuthenticantion.Presentation/MvcCookieAuthenticantion.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcCookieAuthenticantion.Presentation.Models;
+
+namespace MvcCookieAuthenticantion.Presentation.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var name = User.Identity.Name;
+            var claims = User.Claims;
+
+
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel entity)
+        {
+            if (ModelState.IsValid)
+            {
+                var isValid = true; // TODO Validate the username and the password with your own logic
+                if (!isValid)
+                {
+                    ModelState.AddModelError("", "username or password is invalid");
+                    //return Page();
+                }
+
+                // Create the identity from the user info
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "hbarroco"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Helio Barroco"));
+                // You can add roles to use role-based authorization
+                identity.AddClaim(new Claim(ClaimTypes.Role, "Usuário"));
+
+                // Authenticate using the identity
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
